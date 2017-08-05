94f037998a7332ade5dd6bbb31151a61d0c7bda3
Lucas <santaguida1@outlook.com>
2017-08-05 14:15:02 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//import modules
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+var path = require('path');
+
+var app = express();
+
+const route = require('./routes/route');
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017:contactlist');
+
+//on connection
+mongoose.connection.on('connected', ()=>{
+  console.log('Connected to database mongodb @ 27017');
+})
+
+mongoose.connection.on('error', (err)=>{
+  if(err){
+    console.log('Error in Database Connection:' + err);
+  }
+})
+
+//port number
+const port = 3000;
+
+//adding middleWare
+app.use(cors());
+
+//body - parser
+app.use(bodyparser.json());
+
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/api/', route);
+
+//testing Server
+app.get('/', (req,res)=>{
+  res.send('foobar');
+});
+
+app.listen(port,()=>{
+  console.log('Server started at port: '+ port)
+});
